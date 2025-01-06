d86bfc716d849de7c9bd8d7c6513e980623260cd
Ray Lopez <raylopez509@gmail.com>
2025-01-05 17:43:42 -0800

    added book constructor and adding to library

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hi");
+const myLibrary = [];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+let test = new Book("test", "ray");
+let test2 = new Book("test", "ray");
+
+addBookToLibrary(test);
+addBookToLibrary(test2);
+
+for (book of myLibrary) {
+  const card = document.createElement("div");
+  const title = document.createElement("div");
+  title.textContent = book.title;
+  const author = document.createElement("div");
+  author.textContent = book.author;
+  card.appendChild(title);
+  card.appendChild(author);
+  const main = document.querySelector(".main");
+  main.appendChild(card);
+}
