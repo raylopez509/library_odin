af4ffc940be2b179d96313afc61c7925cb43caa2
raylopez509 <raylopez509@gmail.com>
2025-01-08 14:33:06 -0800

    Added ability to add a new book and delete all book cards
    Added createBookCard to create new book and add it to Library and display the cards. Seems a bit inefficient because it deletes all of the cards and recreates them.
    Also added a button to delete all buttons

--- script.js
+++ script.js
@@ -23,49 +23,45 @@ let book2 = new Book(
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+displayCards();
 
-for (book of myLibrary) {
-  const card = document.createElement("div");
-  const title = document.createElement("div");
-  const author = document.createElement("div");
-  const genre = document.createElement("div");
-  const pages = document.createElement("div");
-  const isRead = document.createElement("div");
-  title.textContent = book.title;
-  author.textContent = book.author;
-  genre.textContent = book.genre;
-  pages.textContent = book.pages;
-  if (book.isRead) {
-    isRead.textContent = "Read";
-  } else {
-    isRead.textContent = "Unread";
+function displayCards() {
+  for (book of myLibrary) {
+    const card = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const genre = document.createElement("div");
+    const pages = document.createElement("div");
+    const isRead = document.createElement("div");
+    title.textContent = book.title;
+    author.textContent = book.author;
+    genre.textContent = book.genre;
+    pages.textContent = book.pages;
+    if (book.isRead) {
+      isRead.textContent = "Read";
+    } else {
+      isRead.textContent = "Unread";
+    }
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(genre);
+    card.appendChild(pages);
+    card.appendChild(isRead);
+    const main = document.querySelector(".main");
+    main.appendChild(card);
   }
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(genre);
-  card.appendChild(pages);
-  card.appendChild(isRead);
-  const main = document.querySelector(".main");
-  main.appendChild(card);
 }
+
 
 const dialog = document.querySelector("dialog");
 
 const addBookButton = document.querySelector("#add");
 const confirmAddButton = document.querySelector("#modal-add");
-
-// dialog.addEventListener("close", (e) => {
-//   dialog.close();
-// });
+const cancelButton = document.querySelector("#cancel");
 
 addBookButton.addEventListener("click", () => {
   dialog.showModal();
 });
-
-// confirmAddButton.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   dialog.close();
-// });
 
 dialog.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -74,11 +70,30 @@ dialog.addEventListener("submit", (event) => {
   let genre = document.querySelector(".genre").value;
   let pages = document.querySelector(".pages").value;
   let read = document.querySelector(".read").checked;
-  // if (document.querySelector(".read").value == "Read") {
-  //   test = "read";
-  // } else {
-  //   test = "unread";
-  // }
-  console.log(read);
+  createBookCard(title, author, genre, pages, read);
   dialog.close();
+});
+
+cancelButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  dialog.close();
+});
+
+function createBookCard(title, author, genre, pages, isRead) {
+  let book = new Book(title, author, genre, pages, isRead);
+  addBookToLibrary(book);
+  const main = document.querySelector(".main");
+  while(main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  displayCards();
+}
+
+const deleteButton = document.querySelector("#delete");
+
+deleteButton.addEventListener("click", () => {
+  const main = document.querySelector(".main");
+  while(main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
 });
