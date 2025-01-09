773c45d961fa6b97e766854b5fba02f2d4ce5be6
raylopez509 <raylopez509@gmail.com>
2025-01-08 16:14:22 -0800

    Added delete button and change read status button

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function Book(title, author, genre, pages, isRead) {
   this.genre = genre;
   this.pages = pages;
   this.isRead = isRead;
+  this.index = undefined;
+
 }
 
 function addBookToLibrary(book) {
+  let index = myLibrary.length;
+  book.index = index;
   myLibrary.push(book);
+  console.log(book.index);
 }
+
+
 
 let book1 = new Book("1984", "George Orwell", "Dystopian", 328, true);
 let book2 = new Book(
@@ -25,30 +32,57 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 displayCards();
 
-function displayCards() {
-  for (book of myLibrary) {
-    const card = document.createElement("div");
-    const title = document.createElement("div");
-    const author = document.createElement("div");
-    const genre = document.createElement("div");
-    const pages = document.createElement("div");
-    const isRead = document.createElement("div");
-    title.textContent = book.title;
-    author.textContent = book.author;
-    genre.textContent = book.genre;
-    pages.textContent = book.pages;
+function createCard(book) {
+
+  const main = document.querySelector(".main");
+
+  const card = document.createElement("div");
+  main.appendChild(card);
+
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const genre = document.createElement("div");
+  const pages = document.createElement("div");
+  const isRead = document.createElement("div");
+  title.textContent = book.title;
+  author.textContent = book.author;
+  genre.textContent = book.genre;
+  pages.textContent = book.pages;
+  if (book.isRead) {
+    isRead.textContent = "Read";
+  } else {
+    isRead.textContent = "Unread";
+  }
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(genre);
+  card.appendChild(pages);
+  card.appendChild(isRead);
+
+  const deleteButton = document.createElement("button");
+
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    main.removeChild(card);
+  });
+  card.appendChild(deleteButton);
+
+  const readButton = document.createElement("button");
+  readButton.textContent = "Change Read Status";
+  readButton.addEventListener("click", () => {
+    book.isRead = !book.isRead;
     if (book.isRead) {
       isRead.textContent = "Read";
     } else {
       isRead.textContent = "Unread";
     }
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(genre);
-    card.appendChild(pages);
-    card.appendChild(isRead);
-    const main = document.querySelector(".main");
-    main.appendChild(card);
+  });
+  card.appendChild(readButton);
+}
+
+function displayCards() {
+  for (book of myLibrary) {
+    createCard(book);
   }
 }
 
@@ -82,18 +116,12 @@ cancelButton.addEventListener("click", (event) => {
 function createBookCard(title, author, genre, pages, isRead) {
   let book = new Book(title, author, genre, pages, isRead);
   addBookToLibrary(book);
-  const main = document.querySelector(".main");
-  while(main.firstChild) {
-    main.removeChild(main.firstChild);
-  }
-  displayCards();
+  createCard(book);
 }
 
 const deleteButton = document.querySelector("#delete");
 
 deleteButton.addEventListener("click", () => {
   const main = document.querySelector(".main");
-  while(main.firstChild) {
-    main.removeChild(main.firstChild);
-  }
+  main.parentNode.removeChild(main);
 });
